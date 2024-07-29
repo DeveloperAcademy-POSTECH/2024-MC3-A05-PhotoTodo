4619819593531bd90816db0d4561cb33c93eede1
Lyosha's MacBook <lyoshasmacbook@Lyoshas-MacBook.local>
2024-07-29 15:44:21 +0900

    Init: [dev] DashBoardView 파일 생성

---
+++ PhotoTodo/DashBoardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashBoardView.swift
+//  PhotoTodo
+//
+//  Created by Lyosha's MacBook   on 7/29/24.
+//
+
+import SwiftUI
+
+struct DashBoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashBoardView()
+}
