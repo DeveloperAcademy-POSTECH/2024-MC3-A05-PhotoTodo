cc6d13fa6fd593032e506006f73ef3cd630c61a8
Lyosha's MacBook <lyoshasmacbook@Lyoshas-MacBook.local>
2024-07-29 16:15:59 +0900

    Feat: [dev] FolderListView에 SwiftData 기본 기능 추가

--- PhotoTodo/FolderListView.swift
+++ PhotoTodo/FolderListView.swift
@@ -6,13 +6,56 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct FolderListView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView {
+            List {
+                ForEach(items) { item in
+                    NavigationLink {
+                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                    } label: {
+                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                ToolbarItem {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+        } detail: {
+            Text("Select an item")
+        }
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
     }
 }
 
 #Preview {
     FolderListView()
+        .modelContainer(for: Item.self, inMemory: true)
 }
