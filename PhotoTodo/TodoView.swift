7aeabbb3fae7b9f220f0e793a96527ecdba49a4f
Lyosha's MacBook <lyoshasmacbook@Lyoshas-MacBook.local>
2024-07-29 12:20:38 +0900

    Init: [dev] 뷰 파일 생성

---
+++ PhotoTodo/TodoView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoView.swift
+//  PhotoTodo
+//
+//  Created by Lyosha's MacBook   on 7/29/24.
+//
+
+import SwiftUI
+
+struct TodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoView()
+}
