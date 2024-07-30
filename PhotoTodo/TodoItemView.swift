e7a1853101184391525c671e4b8c58192dbc1c16
Lyosha's MacBook <lyoshasmacbook@10-141-41-101.0101.pos.kr.iosda.org>
2024-07-30 22:59:13 +0900

    Init: [feature/FolderListView] TodoItemView 파일 생셩

---
+++ PhotoTodo/TodoItemView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoItemView.swift
+//  PhotoTodo
+//
+//  Created by Lyosha's MacBook   on 7/30/24.
+//
+
+import SwiftUI
+
+struct TodoItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoItemView()
+}
