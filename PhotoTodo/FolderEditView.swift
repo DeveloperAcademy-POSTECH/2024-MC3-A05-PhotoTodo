695a2ad1d043a8eac4b7115b1c839b95c84a8ff2
jiae0119 <ca090434@naver.com>
2024-08-02 11:40:18 +0900

    Init: [feature/folderCreateView]
    FolderEditView 스위프트UI 파일 생성

---
+++ PhotoTodo/FolderEditView.swift
@@ -0,0 +1,18 @@
+//
+//  FolderEditView.swift
+//  PhotoTodo
+//
+//  Created by JiaeShin on 8/2/24.
+//
+
+import SwiftUI
+
+struct FolderEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FolderEditView()
+}
