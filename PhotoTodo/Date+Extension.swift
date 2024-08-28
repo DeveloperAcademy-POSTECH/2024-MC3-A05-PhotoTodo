e0e68d041a5399c5913998b1e68fa193e6a785d2
Hyungeol94 <ekfrhkquf12@naver.com>
2024-08-28 14:42:29 +0900

    Refactor: [feature/TodoItemView] dayOfYear, daysPassedSinceJanuaryFirst2024, extractYear를 Date+Extension 파일으로 옮김

--- PhotoTodo/Date+Extension.swift
+++ PhotoTodo/Date+Extension.swift
@@ -15,3 +15,18 @@ extension Date {
         return formatter.string(from: alarmData)
     }
 }
+
+func dayOfYear(from date: Date) -> Int {
+    let calendar = Calendar.current
+    return calendar.ordinality(of: .day, in: .year, for: date) ?? 0
+}
+          
+func daysPassedSinceJanuaryFirst2024(from date: Date) -> Int {
+  return dayOfYear(from : date) + (extractYear(from : date)-2024)*365
+}
+
+func extractYear(from date: Date) -> Int {
+  let calendar = Calendar.current
+  let year = calendar.component(.year, from: date)
+  return year
+}
