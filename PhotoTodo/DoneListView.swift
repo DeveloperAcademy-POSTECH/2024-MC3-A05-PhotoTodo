75a360da510ed3032d9fa34e4ed04923467035f8
Hyungeol94 <ekfrhkquf12@naver.com>
2024-10-25 12:30:56 +0900

    Feat: [Design] 폴더에서 진입 시 나오는 뷰들에 배경색 추가

--- PhotoTodo/DoneListView.swift
+++ PhotoTodo/DoneListView.swift
@@ -12,43 +12,48 @@ struct DoneListView: View {
     
     var body: some View {
         //TODO: 배너 넣기
-        NavigationLink{
-            DashboardView()
-        } label : {
-            RoundedRectangle(cornerRadius: 20)
-                .fill(.white)
-                .shadow(color: .paleGray , radius: 8)
-                .frame(minHeight: 25, maxHeight: 85)
-                .overlay(
-                    HStack{
-                        VStack{
+        ZStack{
+            Color("gray/gray-200").ignoresSafeArea()
+            VStack{
+                NavigationLink{
+                    DashboardView()
+                } label : {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.white)
+                        .shadow(color: .paleGray , radius: 8)
+                        .frame(minHeight: 25, maxHeight: 85)
+                        .overlay(
                             HStack{
-                                Text("지금까지 네잎클로버 ").foregroundStyle(.black).bold().font(.system(size: 20)) +
-                                Text("\(deletionCount / 4)개").foregroundStyle(.green).bold().font(.system(size: 20)) +
-                                Text("를 모았어요!").foregroundStyle(.black).bold().font(.system(size: 20))
-                                Spacer()
-                            }
-                            .padding(.leading, 12)
-                            .padding(.bottom, 3)
-                            
-                            HStack{
-                                Image(systemName: "info.circle.fill")
-                                    .foregroundStyle(Color("gray/gray-700"))
-                                    .font(.system(size: 14))
+                                VStack{
+                                    HStack{
+                                        Text("지금까지 네잎클로버 ").foregroundStyle(.black).bold().font(.system(size: 20)) +
+                                        Text("\(deletionCount / 4)개").foregroundStyle(.green).bold().font(.system(size: 20)) +
+                                        Text("를 모았어요!").foregroundStyle(.black).bold().font(.system(size: 20))
+                                        Spacer()
+                                    }
+                                    .padding(.leading, 12)
+                                    .padding(.bottom, 3)
                                     
-                                Text("완료한 사진을 지우면 탄소배출을 줄일 수 있어요")
-                                    .foregroundStyle(Color("gray/gray-700"))
-                                    .font(.system(size: 14))
-                                Spacer()
-                            }.padding(.leading, 12)
-                        }
-                        Image(systemName: "chevron.right")
-                    }
-                    .padding(16)
-                )
-                .padding()
+                                    HStack{
+                                        Image(systemName: "info.circle.fill")
+                                            .foregroundStyle(Color("gray/gray-700"))
+                                            .font(.system(size: 14))
+                                            
+                                        Text("완료한 사진을 지우면 탄소배출을 줄일 수 있어요")
+                                            .foregroundStyle(Color("gray/gray-700"))
+                                            .font(.system(size: 14))
+                                        Spacer()
+                                    }.padding(.leading, 12)
+                                }
+                                Image(systemName: "chevron.right")
+                            }
+                            .padding(16)
+                        )
+                        .padding()
+                }
+                TodoCompositeGridView(viewType: .doneList)
+            }
         }
-        TodoCompositeGridView(viewType: .doneList)
     }
 }
 
