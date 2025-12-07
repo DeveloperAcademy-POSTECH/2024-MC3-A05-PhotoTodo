15883eef0e1c151ffcbd19fd3408ba7252d8ad5c
Hyungeol94 <ekfrhkquf12@naver.com>
2025-12-07 16:52:07 +0900

    feat: PhotoTodoMigrationPlan 구현

--- PhotoTodo/Models/PhotoTodoMigrationPlan.swift
+++ PhotoTodo/Models/PhotoTodoMigrationPlan.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftData
+import SwiftUI
 
 enum PhotoTodoMigrationPlan: SchemaMigrationPlan {
     static var schemas: [any VersionedSchema.Type] {
@@ -15,12 +16,29 @@ enum PhotoTodoMigrationPlan: SchemaMigrationPlan {
     static var stages: [MigrationStage] {
         [migrateV1toV2]
     }
+    static var rawImageBackup: [UUID: [Data]] = [:]
     
     static let migrateV1toV2 = MigrationStage.custom(
         fromVersion: PhotoTodoSchemaV1.self,
         toVersion: PhotoTodoSchemaV2.self,
         willMigrate: { context in
-            print("migrate!")
-        }, didMigrate: nil
+            let todos = try? context.fetch(FetchDescriptor<PhotoTodoSchemaV1.Todo>())
+            todos?.forEach { todo in
+                rawImageBackup[todo.id] = todo.images
+            }
+            try? context.save()
+        },
+        didMigrate: { context in
+            let todos = try? context.fetch(FetchDescriptor<PhotoTodoSchemaV2.Todo>())
+            todos?.forEach { todo in
+                if let imageData = rawImageBackup[todo.id] {
+                    todo.images = imageData.map { image in
+                        PhotoTodoSchemaV2.Photo(image: image)
+                    }
+                }
+            }
+            try? context.save()
+            rawImageBackup.removeAll()
+        }
     )
 }
